a57259b0b9b79421883a02ba0cfbc2a0cbfb5f02
farhanaripa <farhana.nwu.spr18@gmail.com>
2020-03-01 00:41:36 +0600

    image change

--- app/src/main/java/com/example/nwu/phonegallery/MainActivity.java
+++ app/src/main/java/com/example/nwu/phonegallery/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<Integer> mImageIds = new ArrayList < >(Arrays.asList(
             R.drawable.img1,R.drawable.img2,R.drawable.img3,R.drawable.img4,
             R.drawable.img5,R.drawable.img6,R.drawable.img7,R.drawable.img8,
-            R.drawable.img9,R.drawable.img10,R.drawable.img11,R.drawable.img12));
+            R.drawable.img9,R.drawable.img10,R.drawable.img11));
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
