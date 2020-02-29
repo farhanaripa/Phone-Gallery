850d17bc865f97fea15159ec515c0934d9b3e181
farhanaripa <farhana.nwu.spr18@gmail.com>
2020-03-01 02:24:22 +0600

    image change

--- app/src/main/java/com/example/nwu/phonegallery/FullView.java
+++ app/src/main/java/com/example/nwu/phonegallery/FullView.java
@@ -1,4 +1,21 @@
 package com.example.nwu.phonegallery;
 
-public class FullView {
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FullView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.full_view);
+
+        ImageView imageView = findViewById(R.id.img_full);
+
+        int img_id = getIntent().getExtras().getInt("img_id");
+        imageView.setImageResource(img_id);
+    }
 }
---
+++ app/src/main/java/com/example/nwu/phonegallery/SplashActivity.java
@@ -0,0 +1,35 @@
+package com.example.nwu.phonegallery;
+
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Thread thread =new Thread() {
+
+            @Override
+            public void run() {
+
+                try {
+                    sleep(3000);
+                }
+                catch (Exception e) {}
+                finally
+                {
+                    startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                    finish();
+                }
+            }
+        };
+
+        thread.start();
+    }
+
+}
